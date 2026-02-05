b34ccd5243b25cc97425c77c9dc4953d0b89263d
guenaud-ephram-emf <ephram.guenaud@studentfr.ch>
2026-02-05 09:51:23 +0100

    Boutton couleur

--- src/frontend/app.js
+++ src/frontend/app.js
@@ -110,3 +110,11 @@ $(document).ready(function () {
     }
   }
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const toggleBtn = document.getElementById("theme-toggle");
+
+  toggleBtn.addEventListener("click", () => {
+    document.body.classList.toggle("alt-theme");
+  });
+});
